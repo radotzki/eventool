7b2d1f7c3d4788fd79a8e63c1650c75027fd1ad9
Itay Radotzki <radotzki@gmail.com>
2014-07-30 13:46:21 +0300

    Use Postgres instead of MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140722130137) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "client_comments", force: true do |t|
     t.integer  "user_id"
     t.integer  "client_id"

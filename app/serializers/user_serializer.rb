f800d16b8b644a6917f580b1fbed809d610203d8
Itay Radotzki <radotzki@gmail.com>
2014-09-03 11:43:46 +0300

    Add 'lock' to user serialaizer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :production_id, :first_name, :last_name, :email, :role, :phone_number
+  attributes :id, :production_id, :first_name, :last_name, :email, :role, :phone_number, :lock
 end

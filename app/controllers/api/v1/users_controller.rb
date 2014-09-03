7503f40b87b2ea0d93ec6e26c4c24d51dd5689a8
Itay Radotzki <radotzki@gmail.com>
2014-09-03 11:57:29 +0300

    Change POST user to accept user's role

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -82,7 +82,7 @@ class API::V1::UsersController < ApplicationController
    private
 
     def user_params
-      params.permit(:production_id, :first_name, :last_name, :email, :password ,:phone_number)
+      params.permit(:production_id, :first_name, :last_name, :email, :password ,:phone_number, :role)
     end
 
 end

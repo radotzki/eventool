4a2034bfa2b0c11c9800c404fbe0bda57bab042e
Itay Radotzki <radotzki@gmail.com>
2014-10-12 20:29:49 +0300

    try CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  rescue_from CanCan::AccessDenied do |exception|
-    render json: {:alert => exception.message}, status: :forbidden
+rescue_from CanCan::AccessDenied do |exception|
+  render json: {:alert => exception.message}, status: :forbidden
+end
+
+private
+
+def restrict_access
+  token = request.headers["token"]
+  user = token && User.find_by_authentication_token(token.to_s)
+  if user
+    sign_in user, store: false
+  else
+    render json: {}, status: :unauthorized
   end
-
-   private
-
-    def restrict_access
-      token = request.headers["token"]
-      user = token && User.find_by_authentication_token(token.to_s)
-      if user
-        sign_in user, store: false
-      else
-        render json: {}, status: :unauthorized
-      end
-    end
+end
 
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :password, ask('Server password', nil)
 
-server 'amitay.cloudapp.net', user: 'eventool', roles: %w{web app}, password: fetch(:password)
+server 'amitay.cloudapp.net', user: 'root', roles: %w{web app}, password: fetch(:password)

829b3f0313b6009f1ea205ef0dbb775b24af8559
Itay Radotzki <radotzki@gmail.com>
2014-11-19 19:25:11 +0200

    friendship serializer: return also client one

--- app/serializers/client_friendship_serializer.rb
+++ app/serializers/client_friendship_serializer.rb
@@ -1,3 +1,3 @@
 class ClientFriendshipSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :client_one_id, :client_two
+  attributes :id, :user_id, :client_one, :client_two, :created_at
 end
